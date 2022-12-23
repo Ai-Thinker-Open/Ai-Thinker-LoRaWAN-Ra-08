0545720705f405344f6a9ae2b69e4b9cbea6b3ba
saka <876038559@qq.com>
2022-12-23 13:45:43 +0800

    添加冷暖灯交替闪烁效果

--- projects/ASR6601CB-EVAL/tutorial/light/src/main.c
+++ projects/ASR6601CB-EVAL/tutorial/light/src/main.c
@@ -1,19 +1,65 @@
 #include <string.h>
 
 #include "init.h"
+#include "tremo_delay.h"
 
+#define LIGHT_WARM GPIO_PIN_15
+#define LIGHT_COLD GPIO_PIN_14
+
+/**
+ * @brief Setup the board,enable GPIO
+ */
 void setup_board()
 {
-    const rcc_peripheral_t peripheral_array[] = {RCC_PERIPHERAL_UART0, RCC_PERIPHERAL_GPIOA};
-    board_init(peripheral_array,sizeof(peripheral_array)/sizeof(rcc_peripheral_t));
+    const rcc_peripheral_t peripheral_array[] = {
+        RCC_PERIPHERAL_UART0, RCC_PERIPHERAL_GPIOA,
+        RCC_PERIPHERAL_GPIOB};
+    board_init(peripheral_array, sizeof(peripheral_array) / sizeof(rcc_peripheral_t));
+}
+
+/**
+ * @brief Setup the warm and colde light,which will off
+ *
+ */
+void setup_light()
+{
+    gpio_init(GPIOA, LIGHT_COLD, GPIO_MODE_OUTPUT_PP_LOW);
+    gpio_init(GPIOA, LIGHT_WARM, GPIO_MODE_OUTPUT_PP_LOW);
+}
+
+/**
+ * @brief turn warm light on
+ * 
+ */
+void turn_warm_light_on()
+{
+    gpio_write(GPIOA, LIGHT_COLD, GPIO_LEVEL_LOW);
+    gpio_write(GPIOA, LIGHT_WARM, GPIO_LEVEL_HIGH);
+}
+
+/**
+ * @brief turn cold light on
+ * 
+ */
+void turn_cold_light_on()
+{
+    gpio_write(GPIOA, LIGHT_COLD, GPIO_LEVEL_HIGH);
+    gpio_write(GPIOA, LIGHT_WARM, GPIO_LEVEL_LOW);
 }
 
 int main(void)
 {
     setup_board();
-    printf("start test light");
+    uart_log_init();
+    printf("start test light\n");
+    setup_light();
+    //loop , cold and warm light on or off
     while (1)
     {
+        turn_cold_light_on();
+        delay_ms(1000);
+        turn_warm_light_on();
+        delay_ms(1000);
     }
 }
 
